5b20ad9436a4e642c6b5bdb10f195516f4e333f0
Rian Oliveira <riandiasdeoliveira2001@gmail.com>
2023-01-02 00:17:39 -0300

    update

---
+++ src/components/screen/stage/BlankVote.tsx
@@ -0,0 +1,20 @@
+import { PoliticalOffice } from "../candidate/PoliticalOffice";
+import { Footer } from "../Footer";
+import { Header } from "../Header";
+
+export const BlankVote = (): JSX.Element => {
+  return (
+    <>
+      <Header />
+      <div className="translate-x-[80px] translate-y-[16px]">
+        <PoliticalOffice />
+      </div>
+      <div className="translate-x-[100px] translate-y-[56px]">
+        <span className="text-[36px] animate-fast-pulse">VOTO EM BRANCO</span>
+      </div>
+      <div className="translate-y-[115px]">
+        <Footer />
+      </div>
+    </>
+  );
+};
