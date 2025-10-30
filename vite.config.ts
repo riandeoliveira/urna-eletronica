97bd05a5e02ce4b1ff2f6a3e42d6cef8facbec2f
Rian Oliveira <riandiasdeoliveira2001@gmail.com>
2025-10-30 13:04:58 -0300

    chore: minor changes

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,10 @@
 import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
-import { VitePWA } from "vite-plugin-pwa";
 import svgr from "vite-plugin-svgr";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  plugins: [react(), svgr(), tsconfigPaths(), VitePWA()],
+  plugins: [react(), svgr(), tsconfigPaths()],
   test: {
     environment: "jsdom",
   },
